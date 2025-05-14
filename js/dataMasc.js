29b422b165c68f7bfec9a432aacb18f3162554c3
Fernando Grabobi <85689008+FernandoGrabobi@users.noreply.github.com>
2025-05-14 17:42:02 -0300

    cambios en los planteles

--- js/dataMasc.js
+++ js/dataMasc.js
@@ -1,26 +1,31 @@
 const teamsData = {
     a1: {
-        title: "PLANTEL A1",
+        title: "PLANTEL A1 A",
         players: [
             {
                 name: "Ricardo Manuel Sosa",
                 position: "Director Técnico",
-                image: "/assets/Fotos Jugadores/A1-Masculino/manuel.jpg"
+                image: "/assets/imagenes/dts/manuelSosa.jpg"
             },
             {
                 name: "Rodriguez Luciano Jesus",
                 position: "Armador / 1.80mts",
-                image: "/assets/Fotos Jugadores/A1-Masculino/Luciano.jpg"
+                image: "/assets/imagenes/A1-Masculino/A/lucianoR.jpg"
             },
             {
                 name: "Campoy Leandro Manuel",
                 position: "Armador / 1.65mts",
-                image: "/assets/Fotos Jugadores/A1-Masculino/Colo.jpg"
+                image: "/assets/imagenes/A1-Masculino/A/leandroCampoy.jpg"
             },
             {
                 name: "Gimenez Gabriel Rodrigo",
                 position: "Opuesto / 1.84mts",
-                image: "/assets/Fotos Jugadores/A1-Masculino/Gabriel.jpg"
+                image: "/assets/imagenes/A1-Masculino/A/gabrielGimenez.jpg"
+            },
+                 {
+                name: "Isaias José Daniel Tapia",
+                position: "Opuesto / 1.93mts",
+                image: "/assets/imagenes/A1-Masculino/A/isaTapia2.jpg"
             },
             {
                 name: "Carranza Juan Manuel",
@@ -28,34 +33,29 @@ const teamsData = {
                 image: "/assets/Fotos Jugadores/A1-Masculino/Juan.jpg"
             },
             {
+                name: "Grabobi Fernando Martin",
+                position: "Central / 1.90mts",
+                image: "/assets/imagenes/A1-Masculino/A/ferGrabobi.jpg"
+            },
+            {
                 name: "Muzevich Agustín Franco",
                 position: "Central / 1.87mts",
-                image: "/assets/Fotos Jugadores/A1-Masculino/Sopa.jpg"
+                image: "/assets/imagenes/A1-Masculino/A/agustinM.jpg"
             },
             {
                 name: "Lucero Leonardo",
-                position: "Punta Receptor/Central / 1.74mts",
-                image: "/assets/Fotos Jugadores/A1-Masculino/Leo.jpg"
+                position: "Punta Receptor / 1.74mts",
+                image: "/assets/imagenes/A1-Masculino/A/leoLucero.jpg"
             },
             {
                 name: "Giraudi Marcos",
                 position: "Punta Receptor / 1.77mts",
-                image: "/assets/Fotos Jugadores/A1-Masculino/Marcos.jpg"
-            },
-            {
-                name: "Medina Mateo",
-                position: "Punta Receptor / 1.77mts",
-                image: "/assets/Fotos Jugadores/A1-Masculino/mateo.jpg"
+                image: "/assets/imagenes/A1-Masculino/A/marcosCrino.jpg"
             },
             {
                 name: "Lucero Michael David",
                 position: "Punta Receptor / 1.75mts",
                 image: "/assets/Fotos Jugadores/A1-Masculino/Maicol.jpg"
-            },
-            {
-                name: "Leguizamon Matias",
-                position: "Libero / 1.75mts",
-                image: "/assets/Fotos Jugadores/A1-Masculino/leguizamon.jpg"
             },
             {
                 name: "Villegas Matias",
@@ -64,83 +64,13 @@ const teamsData = {
             }
         ]
     },
-    a2a: {
-        title: 'PLANTEL A2 "A"',
-        players: [
-            {
-                name: "Pablo Romero",
-                position: "Director Técnico",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/pablo.jpg"
-            },
-            {
-                name: "Lopez Laureano Alejandro",
-                position: "Armador / 1.78mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/laureano.jpg"
-            },
-            {
-                name: "Olguin Lucca Alejandro",
-                position: "Armador/Punta Receptor 1.75mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/lukitas.jpg"
-            },
-            {
-                name: "Rodas Jesus Diego",
-                position: "Opuesto / 1.80mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/diego.jpg"
-            },
-            {
-                name: "Quiroga Federico",
-                position: "Opuesto/Punta Receptor 1.82mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/fede.jpg"
-            },
-            {
-                name: "Román Abel",
-                position: "Opuesto/Punta Receptor 1.75mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/abel2.jpg"
-            },
-            {
-                name: "Grabobi Fernando Martin",
-                position: "Opuesto/Central / 1.90mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/fer2.jpg"
-            },
-            {
-                name: "Sosa Cristian",
-                position: "Central / 1.82mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/cristian.jpg"
-            },
-            {
-                name: "Zuviria Javier Nicolas",
-                position: "Central / 1.78mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/zuvi.jpg"
-            },
-            {
-                name: "Maldonado Kevin Leonel",
-                position: "Punta Receptor / 1.73mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/kevin2.jpg"
-            },
-            {
-                name: "Iglesias Jorge Gabriel",
-                position: "Punta Receptor / 1.80mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/jorgito.jpg"
-            },
-            {
-                name: "Moya Leandro Roberto",
-                position: "Libero / 1.71mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/moyita.jpg"
-            },
-            {
-                name: "Acosta Brian Daniel",
-                position: "Libero / 1.66mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-A/brian.jpg"
-            }
-        ]
-    },
-    a2b: {
-        title: 'PLANTEL A2 "B"',
+     a1b: {
+        title: 'PLANTEL A1 B',
         players: [
             {
                 name: "Matias Leguizamon",
                 position: "Director Técnico",
-                image: "/assets/Fotos Jugadores/A2-Masculino-B/Mati.JPG"
+                image: "/assets/imagenes/dts/matiasL.jpg"
             },
             {
                 name: "LLanos Fabian Ricardo",
@@ -148,9 +78,9 @@ const teamsData = {
                 image: "/assets/Fotos Jugadores/A2-Masculino-B/fabi.jpg"
             },
             {
-                name: "Alcaraz Lautaro",
-                position: "Armador / 1.72mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-B/lauti.JPG"
+                name: "Damian Methol",
+                position: "Opuesto / 1.75mts",
+                image: "/assets/imagenes/A1-Masculino/B/damianMethol.jpg"
             },
             {
                 name: "Pizzariello Juan Antonio",
@@ -165,42 +95,87 @@ const teamsData = {
             {
                 name: "Muñoz Diego Hernan",
                 position: "Opuesto / 1.77mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-B/diego.jpg"
-            },
-            {
-                name: "Escudero Mauro Alejandro",
-                position: "Central / 1.74mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-B/mauro.jpg"
+                image: "/assets/imagenes/A1-Masculino/B/diego.jpg"
             },
             {
                 name: "Lopez Alberto Gustavo",
                 position: "Central / 1.88mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-B/gustavo.JPG"
+                image: "/assets/imagenes/A1-Masculino/B/gustavoL.jpg"
             },
             {
                 name: "Obregon Nicolas Alberto",
                 position: "Central / 1.79mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-B/nico.jpg"
+                image: "/assets/imagenes/A1-Masculino/B/nicoObregon.jpg"
             },
             {
                 name: "Sosa Manuel Ricardo",
                 position: "Punta Receptor / 1.90mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-B/manu.jpg"
+                image: "/assets/imagenes/dts/manuelSosa.jpg"
             },
             {
                 name: "Lucero Manuel",
                 position: "Punta Receptor / 1.81mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-B/manuel2.JPG"
+                image: "/assets/imagenes/A1-Masculino/B/manuelLombardi.jpg"
             },
-            {
+           /* {
                 name: "Lamas Juan Jose",
                 position: "Punta Receptor / 1.74mts",
                 image: "/assets/Fotos Jugadores/A2-Masculino-B/lamas.jpg"
-            },
+            },*/
             {
                 name: "Hoyos Lucas",
                 position: "Libero / 1.74mts",
-                image: "/assets/Fotos Jugadores/A2-Masculino-B/lucas.JPG"
+                image: "/assets/imagenes/A1-Masculino/B/lucasHoyo2.jpg"
+            }
+        ]
+    },
+    a2: {
+        title: 'PLANTEL A2',
+        players: [
+            {
+               name: "Ricardo Manuel Sosa",
+                position: "Director Técnico",
+                image: "/assets/imagenes/dts/manuelSosa.jpg"
+            },
+            {
+                name: "Alcaraz Lautaro",
+                position: "Armador / 1.72mts",
+                image: "/assets/imagenes/A2-Masculino/lautaro.jpg"
+            },
+            {
+                name: "Lopez Laureano Alejandro",
+                position: "Armador / 1.78mts",
+                image: "/assets/imagenes/A2-Masculino/laureano.jpg"
+            },
+            {
+                name: "Olguin Lucca Alejandro",
+                position: "Armador/Punta Receptor 1.75mts",
+                image: "/assets/imagenes/A2-Masculino/lucasOlg.jpg"
+            },
+            {
+                name: "Román Abel",
+                position: "Opuesto/Punta Receptor 1.75mts",
+                image: "/assets/imagenes/A2-Masculino/abelR.jpg"
+            },
+            {
+                name: "Zuviria Javier Nicolas",
+                position: "Central / 1.78mts",
+                image: "/assets/imagenes/A2-Masculino/zuvi.jpg"
+            },
+            {
+                name: "Maldonado Kevin Leonel",
+                position: "Punta Receptor / 1.73mts",
+                image: "/assets/imagenes/A2-Masculino/kevinM.jpg"
+            },
+            {
+                name: "Iglesias Jorge Gabriel",
+                position: "Punta Receptor / 1.80mts",
+                image: "/assets/Fotos Jugadores/A2-Masculino-A/jorgito.jpg"
+            },
+            {
+                name: "Acosta Brian Daniel",
+                position: "Libero / 1.66mts",
+                image: "/assets/Fotos Jugadores/A2-Masculino-A/brian.jpg"
             }
         ]
     }
